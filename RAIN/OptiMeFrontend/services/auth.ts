99ad6020769915396f2e3ebb22da0dbf7a10c8a1
Nik <mejaknik@gmail.com>
2026-05-06 22:56:01 +0200

    SCRUM-18 Implement registerUser function for user registration

--- RAIN/OptiMeFrontend/services/auth.ts
+++ RAIN/OptiMeFrontend/services/auth.ts
@@ -17,3 +17,27 @@ export async function loginUser(email: string, password: string) {
 
   return data;
 }
+
+export async function registerUser(
+  username: string,
+  email: string,
+  date_of_birth: Date,
+  gender: string,
+  password: string,
+) {
+  const res = await fetch(`${API_URL}/api/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, date_of_birth, gender, password }),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Register failed");
+  }
+
+  return data;
+}
