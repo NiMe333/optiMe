7b1bc040312269e76864394961225722f9e75a28
Nik <mejaknik@gmail.com>
2026-05-13 22:03:12 +0200

    SCRUM-53 Fix socialConnection field and reformat code
    Rename schema field socialConection -> socialConnection to correct typo, and apply consistent code formatting

--- RAIN/OptiMeBackend/models/userModel.js
+++ RAIN/OptiMeBackend/models/userModel.js
@@ -1,32 +1,33 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    gender: { type: String, required: true },
-    dateOfBirth: { type: Date, required: true },
-    username: { type: String, required: false }, //What should the application call you?
-    education: { type: String, required: false },
-    employment: { type: String, required: false },
-    mood: { type: Number, required: false },
-    sleepHours: { type: String, required: false },
-    activity: { type: String, required: false },
-    socialConection: { type: Number, required: false },
-    phoneScreenTime: { type: String, required: false },
-    stress: { type: String, required: false },
-    formFinished: { type: Boolean, required: false}
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  gender: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
+  username: { type: String, required: false }, //What should the application call you?
+  education: { type: String, required: false },
+  employment: { type: String, required: false },
+  mood: { type: Number, required: false },
+  sleepHours: { type: String, required: false },
+  activity: { type: String, required: false },
+  socialConnection: { type: Number, required: false },
+  phoneScreenTime: { type: String, required: false },
+  stress: { type: String, required: false },
+  formFinished: { type: Boolean, required: false },
 });
 
-UserSchema.pre('save', async function(next) { //pre ran before doc is saved to database
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
+UserSchema.pre("save", async function (next) {
+  //pre ran before doc is saved to database
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-UserSchema.methods.comparePassword = function(password) {
-    return bcrypt.compare(password, this.password);
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('user', UserSchema);
+module.exports = mongoose.model("user", UserSchema);
