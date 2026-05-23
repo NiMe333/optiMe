30a7e09b5d9826636f3799a9230bd1f618273069
Enej <239311488+Enej-Vodusek@users.noreply.github.com>
2026-05-23 16:33:04 +0200

    SCRUM-61 Added a new endpoint for data

--- RAIN/OptiMeBackend/controllers/dataController.js
+++ RAIN/OptiMeBackend/controllers/dataController.js
@@ -61,3 +61,64 @@ exports.trackedMetricsData = async function (req, res) {
         });
     }
 }
+
+exports.calculatedScoresData = async function (req, res) {
+    try {
+        
+        const snapShot1 = await SnapshotModel.findOne({
+            userId: req.user.userId,
+            date: new Date("2026-05-14")
+        });
+        const snapShot2 = await SnapshotModel.findOne({
+            userId: req.user.userId,
+            date: new Date("2026-05-13")
+        });
+        const snapShot3 = await SnapshotModel.findOne({
+            userId: req.user.userId,
+            date: new Date("2026-05-12")
+        });
+        const snapShot4 = await SnapshotModel.findOne({
+            userId: req.user.userId,
+            date: new Date("2026-05-11")
+        });
+        const snapShot5 = await SnapshotModel.findOne({
+            userId: req.user.userId,
+            date: new Date("2026-05-10")
+        });
+        const snapShot6 = await SnapshotModel.findOne({
+            userId: req.user.userId,
+            date: new Date("2026-05-09")
+        });
+        const snapShot7 = await SnapshotModel.findOne({
+            userId: req.user.userId,
+            date: new Date("2026-05-08")
+        });
+
+        const sleepAvg = (snapShot1.sleepHours + snapShot2.sleepHours + snapShot3.sleepHours + snapShot4.sleepHours + snapShot5.sleepHours + snapShot6.sleepHours + snapShot7.sleepHours) / 7;
+        const anxietyAvg = (snapShot1.anxiety + snapShot2.anxiety + snapShot3.anxiety + snapShot4.anxiety + snapShot5.anxiety + snapShot6.anxiety + snapShot7.anxiety) / 7;
+        const moodAvg = (snapShot1.mood + snapShot2.mood + snapShot3.mood + snapShot4.mood + snapShot5.mood + snapShot6.mood + snapShot7.mood) / 7;
+        const activityAvg = (snapShot1.steps + snapShot2.steps + snapShot3.steps + snapShot4.steps + snapShot5.steps + snapShot6.steps + snapShot7.steps) / 7;
+        const screentimeAvg = (snapShot1.screenTimeHours + snapShot2.screenTimeHours + snapShot3.screenTimeHours + snapShot4.screenTimeHours + snapShot5.screenTimeHours + snapShot6.screenTimeHours + snapShot7.screenTimeHours) / 7;
+        const stressAvg = (snapShot1.stress + snapShot2.stress + snapShot3.stress + snapShot4.stress + snapShot5.stress + snapShot6.stress + snapShot7.stress) / 7;
+
+        return res.json({
+            success: true,
+            message: "Calculated scores retrieval successfull",
+            sleepAvg,
+            anxietyAvg,
+            moodAvg,
+            activityAvg,
+            screentimeAvg,
+            stressAvg
+        });
+
+    }
+    catch(err)
+    {
+        return res.status(500).json({
+            success: false,
+            message: "Calculated scores retrival failed",
+        });
+    }
+}
+
--- RAIN/OptiMeBackend/routes/dataRoutes.js
+++ RAIN/OptiMeBackend/routes/dataRoutes.js
@@ -5,5 +5,6 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 router.get('/mentalHealthScore', authMiddleware, controller.mentalHealthData);
 router.get('/trackedMetrics', authMiddleware, controller.trackedMetricsData);
+router.get('/calculatedScores', authMiddleware, controller.calculatedScoresData);
 
 module.exports = router;
